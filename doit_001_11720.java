8d038b526c82d7b1a341185f242129c3c5ca49ef
PARKSUNJUNG <61115291+PARKSUNJUNG@users.noreply.github.com>
2022-07-03 22:46:34 +0900

    Add files via upload

--- doit_001_11720.java
+++ doit_001_11720.java
@@ -1,11 +1,28 @@
 package doitCoding_03;
 
+import java.util.Scanner;
+
 public class doit_001_11720 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test_202220703");
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		// 숫자의 개수 받기
+		int num = scanner.nextInt();
+		
+		// 공백 없이 주어진 N개의 숫자
+		String str = scanner.next();
+		
+		int sum = 0; 
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			char ch= str.charAt(i);
+			sum += ch-'0';
+		}
+		
+		System.out.println(sum);
 	}
 
 }
