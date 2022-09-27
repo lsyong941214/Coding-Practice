7a3804444c93a8a6bd26fb3345a49009428febd9
isb <tmdqo9@gmail.com>
2022-09-27 22:34:23 +0900

    31번 문제를 끝으로 Do it 문제 잠정 은퇴

---
+++ Do it/5.탐색/31.k번째 수 구하기.py
@@ -0,0 +1,40 @@
+'''
+이분탐색개념으로 풀어야 한다는 것을 알고 풀어도 도저히 못풀겠다. 답을 봐도 이해가 가지않는다.
+이 문제를 끝으로 이 문제집을 잠시 제껴두려 한다... 문제설명도 별로일뿐더러 문제난이도가 말이안된다고 느껴진다. 물론 코린이 기준이다.
+화가 너무 나서 진행을 못하겠어.. 아무튼 내 실력에 비해 이 문제집은 너무 어려운것 같다.
+
+문제풀이는 아래 포스팅을 보고 그나마 이해할 수 있었다. 문제집보다 훨씬더 설명이 잘 되어있다.
+https://st-lab.tistory.com/281
+'''
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+# 2차원 배열에서 x보다 작거나 같은수
+def solution(x):
+    res = 0
+
+    for i in range(1, n+1):
+        res += min(x//i, n)
+
+    return res
+
+start = 1
+end = k
+while(start < end):
+    mid = (start+end) // 2
+    res = solution(mid)
+
+    if k <= res:
+        end = mid
+    else :
+        start = mid+1
+
+print(start)
+
+'''
+3
+7
+'''
